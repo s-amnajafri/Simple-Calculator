5b5d39477c2d27633e8cc2ed1765d39825dbc2c4
s-amnajafri <amnajafri1795@gmail.com>
2024-11-13 15:36:07 +0500

    Inputs taken from user

---
+++ calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+    float num1, num2;
+    char operation;
+    printf("Enter first number ");
+    scanf("%f",&num1);
+    printf("Enter second number ");
+    scanf("%f",&num2);
+    printf("Enter the operation you want to perform +, -, *,/: ");
+    scanf(" %c",&operation);
+    
