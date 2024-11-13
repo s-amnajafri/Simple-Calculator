50622b39ee3535ced1bec56814592bcec4bbbe69
s-amnajafri <amnajafri1795@gmail.com>
2024-11-13 15:40:11 +0500

    Division feature added

--- calculator.c
+++ calculator.c
@@ -15,6 +15,10 @@ int main(void){
         case '-':
         printf("%.2f - %.2f= %.2f",num1,num2,num1-num2);
         break;
-         case '*':
+        case '*':
         printf("%.2f + %.2f= %.2f",num1,num2,num1*num2);
         break;
+        case '/':
+        printf("%.2f + %.2f= %.2f",num1,num2,num1/num2);
+        break;
+    }
